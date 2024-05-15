a52b7d88e1eb479ada5d8b02fd31356bddf739ca
Md::Arham <arham511624@gmail.com>
2024-05-15 21:10:30 +0600

    Update sparseTable.cpp

--- ds/sparseTable_MIN_MAX.cpp
+++ ds/sparseTable_MIN_MAX.cpp
@@ -2,9 +2,12 @@
 
 #include "bits/stdc++.h"
 
-const int mxN = 500005;
+#define fastIO std::ios::sync_with_stdio(0);std::cin.tie(0)
+#define ll long long int
 
-int sp[mxN][21], sp1[mxN][21];
+const int mxN = 500005, inf = 1000000005;
+
+int ST[mxN][21], ST1[mxN][21];
 
 int lim(int x) {
 	int ans;
@@ -19,29 +22,44 @@ int lim(int x) {
 
 int MIN(int l, int r) {
 	int m = lim(r-l+1);
-	return std::min(sp[l][m], sp[r-(1<<m)+1][m]);
+	return std::min(ST[l][m], ST[r-(1<<m)+1][m]);
 }
 
 int MAX(int l, int r) {
 	int m = lim(r-l+1);
-	return std::max(sp1[l][m], sp1[r-(1<<m)+1][m]);
+	return std::max(ST1[l][m], ST1[r-(1<<m)+1][m]);
 }
 
 signed main() {
-	int N = mxN;
+	// fastIO;
+	int testCases=1;
+	scanf("%lld",&testCases);
+	// std::cin >> testCases;
 	
-	for (int j = 1; j <= 20; j++) {
-    	for (int i = 1; i+(1<<(j))-1 <= N; i++) {
-    		sp[i][j] = std::min(sp[i][j-1], sp[i+(1<<(j-1))][j-1]);
-    		sp1[i][j] = std::max(sp1[i][j-1], sp1[i+(1<<(j-1))][j-1]);
-    	}
+	for (int T = 1; T <= testCases; T++) {
+        int N;
+        scanf("%d", &N);
+        int a[N];
+        for (int &i : a) {
+            scanf("%d", &i);
+        }
+        for (int i = 1; i <= N; i++) {
+            ST[i][0] = ST1[i][0] = a[i];
+        }
+        for (int j = 1; j <= 20; j++) {
+            for (int i = 1; i+(1<<(j))-1 <= N; i++) {
+                ST[i][j] = std::min(ST[i][j-1], ST[i+(1<<(j-1))][j-1]);
+                ST1[i][j] = std::max(ST1[i][j-1], ST1[i+(1<<(j-1))][j-1]);
+            }
+        }
     }
 	
-	//
+	return 0;
 }
 
 /*
 
-TESTED on CF
+1-based indexing
+TESTED on- AtCoder
 
 */
